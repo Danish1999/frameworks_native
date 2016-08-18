695e4b8360ad35348e6cd5fa9f17f7196657ae1c
Adarsh-MR <adarshmr1998@gmail.com>
2016-08-18 14:02:42 +0000

    Revert "inputflinger: Add option to swap app switch and back buttons"
    This reverts commit 59158a791498263539460c5ab61e9d556444a8ec.

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -144,9 +144,6 @@ struct InputReaderConfiguration {
         // The presence of an external stylus has changed.
         CHANGE_EXTERNAL_STYLUS_PRESENCE = 1 << 7,
 
-        // Swap app switch with back button.
-        CHANGE_APP_SWITCH_BUTTONS = 1 << 10,
-
         // All devices must be reopened.
         CHANGE_MUST_REOPEN = 1 << 31,
     };
@@ -234,9 +231,6 @@ struct InputReaderConfiguration {
     // True to show the location of touches on the touch screen as spots.
     bool showTouches;
 
-    // Swap app switch button with back button.
-    bool appSwitchSwapButtonsEnabled;
-
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
             pointerVelocityControlParameters(1.0f, 500.0f, 3000.0f, 3.0f),
@@ -253,9 +247,7 @@ struct InputReaderConfiguration {
             pointerGestureSwipeMaxWidthRatio(0.25f),
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
-            appSwitchSwapButtonsEnabled(false),
-            showTouches(false)
-           { }
+            showTouches(false) { }
 
     bool getDisplayInfo(bool external, DisplayViewport* outViewport) const;
     void setDisplayInfo(bool external, const DisplayViewport& viewport);
@@ -1136,8 +1128,6 @@ private:
 
     int32_t mOrientation; // orientation for dpad keys
 
-    bool mAppSwitchSwapButtons; // swap app switch with back button
-
     Vector<KeyDown> mKeyDowns; // keys that are down
     int32_t mMetaState;
     nsecs_t mDownTime; // time of most recent key down
@@ -1165,8 +1155,6 @@ private:
     bool isKeyboardOrGamepadKey(int32_t scanCode);
 
     void processKey(nsecs_t when, bool down, int32_t scanCode, int32_t usageCode);
-
-    int getAdjustedKeyCode(int keyCode);
 
     ssize_t findKeyDown(int32_t scanCode);
 
